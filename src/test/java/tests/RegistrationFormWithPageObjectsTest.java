cf59077f898ca33968d8d9521c217d946d33f560
Kachanok <108370860+Kachanok@users.noreply.github.com>
2024-01-08 23:05:27 +0300

    Update RegistrationFormWithPageObjectsTest.java

--- src/test/java/tests/RegistrationFormWithPageObjectsTest.java
+++ src/test/java/tests/RegistrationFormWithPageObjectsTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
 import pages.RegistrationPage;
 
 public class RegistrationFormWithPageObjectsTest extends TestBase {
