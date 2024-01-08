19a46b3be5f23c54e14cf72d9e49b9d81adf7c00
AnastasiaF <kachanav1009@gmail.com>
2024-01-09 00:37:30 +0300

    added selenoid.autotests.cloud

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,15 +3,17 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
+
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+
 
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wb/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 }
