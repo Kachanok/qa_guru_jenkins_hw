5e179a51121911ccbab2979fffddb7ec340a6cff
AnastasiaF <kachanav1009@gmail.com>
2024-01-21 15:40:19 +0300

    added browser/webdriver config

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,36 +1,44 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverConfig;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+
 
 
 public class TestBase {
+    final static WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
     @BeforeAll
     static void beforeAll() {
 
 
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.baseUrl = webDriverConfig.baseUrl();
+        Configuration.browser = webDriverConfig.browser();
+        Configuration.browserSize = webDriverConfig.browserSize();
+        Configuration.browserVersion = webDriverConfig.browserVersion();
         Configuration.pageLoadStrategy = "eager";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoidUrl", "selenoid.autotests.cloud") + "/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        if (webDriverConfig.isRemote()) {
+            Configuration.remote = String.valueOf(webDriverConfig.remoteUrl());
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            Configuration.browserCapabilities = capabilities;
+        }
     }
+
 
     @AfterEach
     void addAttachments(){
@@ -39,7 +47,9 @@ public class TestBase {
         Attach.browserConsoleLogs();
         Attach.addVideo();
 
-        Selenide.closeWebDriver();
+        if (Configuration.browser.equals("CHROME")) {
+            Attach.browserConsoleLogs();
+        }
 
     }
 }
